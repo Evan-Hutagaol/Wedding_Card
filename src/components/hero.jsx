0273d19cde3bb073a1a952b3383395898247b972
evanhutagaol <evanhutagaol15@gmail.com>
2023-08-03 01:33:29 +0700

    initial hero

---
+++ src/components/hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+function hero() {
+  return (
+    <div
+      class="hero w-100 h-100 p-3 mx-auto text-center d-flex justify-content-center align-items-center text-white"
+      id="hero"
+    >
+      <main>
+        <h4>Kepada Bapak/Ibu/Saudara/i, </h4>
+        <h1>Evan & Lisa</h1>
+        <p>Akan melangsungkan resepsi pernikahan dalam:</p>
+        <a href="#undangan" class="btn btn-lg mt-4">
+          Lihat Undangan
+        </a>
+      </main>
+    </div>
+  );
+}
+
+export default hero;
