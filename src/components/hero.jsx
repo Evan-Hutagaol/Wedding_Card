dbb06637af61c2e0721d8c80005efb329c41551e
evanhutagaol <evanhutagaol15@gmail.com>
2023-08-04 13:15:00 +0700

    update hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Countdown from "./Countdown";
 
 function hero() {
   return (
@@ -7,10 +7,15 @@ function hero() {
       class="hero w-100 h-100 p-3 mx-auto text-center d-flex justify-content-center align-items-center text-white"
       id="hero"
     >
-      <main>
+      <main class="main">
         <h4>Kepada Bapak/Ibu/Saudara/i, </h4>
         <h1>Evan & Lisa</h1>
         <p>Akan melangsungkan resepsi pernikahan dalam:</p>
+
+        <div class="countdown">
+          <Countdown />
+        </div>
+
         <a href="#undangan" class="btn btn-lg mt-4">
           Lihat Undangan
         </a>
