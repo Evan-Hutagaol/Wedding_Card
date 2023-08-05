359ad4bda97d2061e27c3348fbf0a9e5e6882c56
evanhutagaol <evanhutagaol15@gmail.com>
2023-08-05 22:05:28 +0700

    add Navbar to Homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+
 import "./App.css";
-import Hero from "./components/hero";
+import Hero from "./pages/Home";
+import Navbar from "./components/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return <div className="App">
-    <Hero />
+    <Hero/>
+    <Navbar/>
   </div>;
 }
 
