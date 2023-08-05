4edf618323b68d570057cc08196477843bb7b037
evanhutagaol <evanhutagaol15@gmail.com>
2023-08-05 22:07:14 +0700

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Countdown from "../components/Countdown";
+
+function hero() {
+  return (
+    <div
+      class="hero w-100 h-100 p-3 mx-auto text-center d-flex justify-content-center align-items-center text-white"
+      id="hero"
+    >
+      <main class="main">
+        <h4>Kepada Bapak/Ibu/Saudara/i, </h4>
+        <h1>Evan & Lisa</h1>
+        <p>Akan melangsungkan resepsi pernikahan dalam:</p>
+
+        <div class="countdown">
+          <Countdown />
+        </div>
+
+        <a href="#undangan" class="btn btn-lg mt-4">
+          Lihat Undangan
+        </a>
+      </main>
+    </div>
+  );
+}
+
+export default hero;
