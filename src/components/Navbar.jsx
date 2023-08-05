4ab19f78bc6ff77d339cef865eadd39be8db9955
evanhutagaol <evanhutagaol15@gmail.com>
2023-08-05 22:04:20 +0700

    add Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Navbar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Navbar() {
+  return (
+    <nav class="navbar sticky-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          Offcanvas navbar
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar"
+          aria-controls="offcanvasNavbar"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="offcanvas offcanvas-end"
+          tabindex="-1"
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+        >
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
+              Offcanvas
+            </h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="offcanvas-body">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled">Disabled</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
