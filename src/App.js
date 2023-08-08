39ae49c6b190d04334431ca44eede6e39af94603
evanhutagaol <evanhutagaol15@gmail.com>
2023-08-08 11:36:01 +0700

    add Info Acara

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-
-import "./App.css";
-import Hero from "./pages/Home";
+import Hero from "./pages/Hero";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import InfoAcara from "./components/InfoAcara";
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 function App() {
   return <div className="App">
     <Hero/>
     <Navbar/>
+    <Home/>
+    <InfoAcara/>
   </div>;
 }
 
