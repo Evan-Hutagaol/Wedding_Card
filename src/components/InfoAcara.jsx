6bf89536a948b0b0da788d1c46df82d5fd89fd9b
evanhutagaol <evanhutagaol15@gmail.com>
2023-08-08 13:05:07 +0700

    add some calendar

--- src/components/InfoAcara.jsx
+++ src/components/InfoAcara.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 function InfoAcara() {
   return (
@@ -28,9 +28,56 @@ function InfoAcara() {
             </div>
           </div>
         </div>
+
+        <div className="row justify-content-center">
+          <div className="col-md-4 col-8 mb-4">
+            <div class="card text-center">
+              <div class="card-header">Martuppol</div>
+              <div class="card-body">
+                <div className="row justify-content-center">
+                  <div className="col-md-6">
+                    <i class="bi bi-clock d-block"></i>
+                    <span>09.00 - 11.00</span>
+                  </div>
+                  <div className="col-md-6">
+                    <i class="bi bi-calendar4-week d-block"></i>
+                    <span>Jumat, 1 September 2023</span>
+                  </div>
+                </div>
+              </div>
+
+              <div class="card-footer">
+                Saat acara martuppol dimohon kepada seluruh tamu tetap kondusif
+                pada saat acara berlangsung
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 col-8">
+            <div class="card text-center">
+              <div class="card-header">Pesta Pernikahan</div>
+              <div class="card-body">
+                <div className="row justify-content-center">
+                  <div className="col-md-6">
+                    <i class="bi bi-clock d-block"></i>
+                    <span>12.00 - Selesai</span>
+                  </div>
+                  <div className="col-md-6">
+                    <i class="bi bi-calendar4-week d-block"></i>
+                    <span>Sabtu, 2 September 2023</span>
+                  </div>
+                </div>
+              </div>
+
+              <div class="card-footer">
+                Saat acara pestra pernikahan dimohon kepada seluruh tamu tetap
+                kondusif pada saat acara berlangsung
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
-  )
+  );
 }
 
-export default InfoAcara
+export default InfoAcara;
