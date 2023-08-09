0064be461e38462874251662ef96147b23115c70
evanhutagaol <evanhutagaol15@gmail.com>
2023-08-09 22:45:42 +0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import Hero from "./pages/Hero";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import InfoAcara from "./components/InfoAcara";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App">
-    <Hero/>
-    <Navbar/>
-    <Home/>
-    <InfoAcara/>
-  </div>;
+  return (
+
+      <div className="App">
+        
+
+        <Hero />
+        <Navbar />
+        <Home />
+        <InfoAcara />
+      </div>
+    
+  );
 }
 
 export default App;
